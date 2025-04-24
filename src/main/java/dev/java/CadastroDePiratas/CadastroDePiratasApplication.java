afe7daa5ecb5820403b88a6d60e5584f0ef21fce
Heitor <heitor@tom.tom>
2025-04-24 18:28:39 -0300

    first commit

---
+++ src/main/java/dev/java/CadastroDePiratas/CadastroDePiratasApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDePiratas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePiratasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePiratasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/CadastroDePiratas/CadastroDePiratasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDePiratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePiratasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
